3d7416cfead087f47accfed59fe7ce0ec851253d
Andrei <giura.andrei70@gmail.com>
2025-07-22 11:45:45 +0300

    remove one header

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -72,7 +72,8 @@ export default async function handleRequest(
 
   responseHeaders.set('Cross-Origin-Embedder-Policy', 'require-corp');
   responseHeaders.set('Cross-Origin-Opener-Policy', 'same-origin');
-  responseHeaders.set('Cross-Origin-Resource-Policy', 'cross-origin');
+
+  // responseHeaders.set('Cross-Origin-Resource-Policy', 'cross-origin');
 
   return new Response(body, {
     headers: responseHeaders,
